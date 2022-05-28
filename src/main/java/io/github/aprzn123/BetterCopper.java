43da46103469298c09749406445f939148daba40
aprzn <novaberman@pm.me>
2022-05-28 16:02:52 -0400

    added basic copper block

--- src/main/java/io/github/aprzn123/BetterCopper.java
+++ src/main/java/io/github/aprzn123/BetterCopper.java
@@ -1,8 +1,13 @@
 package io.github.aprzn123;
 
 import com.mojang.serialization.Lifecycle;
+import io.github.aprzn123.block.ConductiveCopper;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -17,6 +22,8 @@ public class BetterCopper implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("copper");
 
+	public static final Block COPPER = new ConductiveCopper(AbstractBlock.Settings.copy(Blocks.COPPER_BLOCK));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -25,6 +32,9 @@ public class BetterCopper implements ModInitializer {
 
 		// Not working for some reason; figure out later
 		//Registry.BLOCK.replace(OptionalInt.of(Registry.BLOCK.getRawId(Blocks.COPPER_BLOCK)), Registry.BLOCK.getKey(Blocks.COPPER_BLOCK).orElseThrow(), new Block(AbstractBlock.Settings.of(Material.WOOL, MapColor.WHITE).strength(0.8F).sounds(BlockSoundGroup.WOOL)), Lifecycle.stable());
+		Registry.register(Registry.BLOCK, new Identifier("copper", "copper"), COPPER);
+		Registry.register(Registry.ITEM, new Identifier("copper", "copper"), new BlockItem(COPPER, new FabricItemSettings().group(ItemGroup.MISC)));
+
 		LOGGER.info("Hello Fabric world!");
 	}
 }
---
+++ src/main/java/io/github/aprzn123/block/ConductiveCopper.java
@@ -0,0 +1,9 @@
+package io.github.aprzn123.block;
+
+import net.minecraft.block.Block;
+
+public class ConductiveCopper extends Block {
+    public ConductiveCopper(Settings settings) {
+        super(settings);
+    }
+}
